f1caf8c812ea988dfec2f3aae35e175091416e3f
андрей китенко <genialniy.kitenko@gmail.com>
2021-05-13 12:51:24 +0300

    deleted class and combined into one function

--- classification_model_git.py
+++ classification_model_git.py
@@ -7,64 +7,43 @@ from classification_models.tfkeras import Classifiers
 from config import NAME_MODEL, INPUT_SHAPE, NUMBER_OF_CLASSES, WEIGHTS
 
 
-class CustomModelGit:
-    def __init__(self, input_shape: Tuple[int, int, int] = INPUT_SHAPE, num_classes: int = NUMBER_OF_CLASSES,
-                 name_model: str = NAME_MODEL, weights: str = WEIGHTS) -> None:
-        """
-        This class creates a model from 'classification_models.tfkeras' or 'efficientnet.tfkeras' library depending on
-        the input parameters.
+def build_model(input_shape: Tuple[int, int, int] = INPUT_SHAPE, num_classes: int = NUMBER_OF_CLASSES,
+                name_model: str = NAME_MODEL, weights: str = WEIGHTS) -> tf.keras.models.Model:
+    """
+    This function creates a model from 'classification_models.tf.keras' or 'efficientnet.tf.keras' library depending on
+    the input parameters.
 
-        :param input_shape: input shape (height, width, channels).
-        :param num_classes: number of classes.
-        :param name_model: the name of the model to be built.
-        :param weights: imageNet or None.
-        """
-        self.input_shape = input_shape
-        self.num_classes = num_classes
-        self.name_model = name_model
-        self.weights = weights
+    :param input_shape: input shape (height, width, channels).
+    :param num_classes: number of classes.
+    :param name_model: the name of the model to be built.
+    :param weights: ImageNet or None.
+    :return: tf.keras.models.Model
+    """
 
-    def build(self):
-        """
-        This function builds the model based on the model name.
+    if NAME_MODEL[:-2].lower() != 'efficientnet':
+        name_model, preprocess_input = Classifiers.get(name_model)
+        base_model = name_model(input_shape=input_shape, weights=weights, include_top=False)
+    elif name_model == 'EfficientNetB0':
+        base_model = efn.EfficientNetB0(input_shape=input_shape, weights=weights, include_top=False)
+    elif name_model == 'EfficientNetB1':
+        base_model = efn.EfficientNetB1(input_shape=input_shape, weights=weights, include_top=False)
+    elif name_model == 'EfficientNetB2':
+        base_model = efn.EfficientNetB2(input_shape=input_shape, weights=weights, include_top=False)
+    elif name_model == 'EfficientNetB3':
+        base_model = efn.EfficientNetB3(input_shape=input_shape, weights=weights, include_top=False)
+    elif name_model == 'EfficientNetB4':
+        base_model = efn.EfficientNetB4(input_shape=input_shape, weights=weights, include_top=False)
+    elif name_model == 'EfficientNetB5':
+        base_model = efn.EfficientNetB5(input_shape=input_shape, weights=weights, include_top=False)
+    elif name_model == 'EfficientNetB6':
+        base_model = efn.EfficientNetB6(input_shape=input_shape, weights=weights, include_top=False)
+    elif name_model == 'EfficientNetB7':
+        base_model = efn.EfficientNetB7(input_shape=input_shape, weights=weights, include_top=False)
+    else:
+        base_model = efn.EfficientNetL2(input_shape=input_shape, weights=weights, include_top=False)
 
-        :return: tf.keras.model
-        """
-        name_model, preprocess_input = Classifiers.get(self.name_model)
-        base_model = name_model(input_shape=self.input_shape, weights=self.weights, include_top=False)
-        x = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
-        output = tf.keras.layers.Dense(self.num_classes, activation='softmax')(x)
-        model = tf.keras.models.Model(inputs=[base_model.input], outputs=[output])
+    x = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
+    output = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+    model = tf.keras.models.Model(inputs=[base_model.input], outputs=[output])
 
-        return model
-
-    def build_efficientnet(self) -> tf.keras.model:
-        """
-        This function builds efficientnet model based on the model name.
-
-        :return: tf.keras.model
-        """
-        if self.name_model == 'EfficientNetB0':
-            base_model = efn.EfficientNetB0(input_shape=self.input_shape, weights=self.weights, include_top=False)
-        elif self.name_model == 'EfficientNetB1':
-            base_model = efn.EfficientNetB1(input_shape=self.input_shape, weights=self.weights, include_top=False)
-        elif self.name_model == 'EfficientNetB2':
-            base_model = efn.EfficientNetB2(input_shape=self.input_shape, weights=self.weights, include_top=False)
-        elif self.name_model == 'EfficientNetB3':
-            base_model = efn.EfficientNetB3(input_shape=self.input_shape, weights=self.weights, include_top=False)
-        elif self.name_model == 'EfficientNetB4':
-            base_model = efn.EfficientNetB4(input_shape=self.input_shape, weights=self.weights, include_top=False)
-        elif self.name_model == 'EfficientNetB5':
-            base_model = efn.EfficientNetB5(input_shape=self.input_shape, weights=self.weights, include_top=False)
-        elif self.name_model == 'EfficientNetB6':
-            base_model = efn.EfficientNetB6(input_shape=self.input_shape, weights=self.weights, include_top=False)
-        elif self.name_model == 'EfficientNetB7':
-            base_model = efn.EfficientNetB7(input_shape=self.input_shape, weights=self.weights, include_top=False)
-        else:
-            base_model = efn.EfficientNetL2(input_shape=self.input_shape, weights=self.weights, include_top=False)
-
-        x = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
-        output = tf.keras.layers.Dense(self.num_classes, activation='softmax')(x)
-        model = tf.keras.models.Model(inputs=[base_model.input], outputs=[output])
-
-        return model
+    return model
