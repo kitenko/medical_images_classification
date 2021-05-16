3aa0ecaa5ecdfff3a8c0be739b6d67e877e96222
андрей китенко <genialniy.kitenko@gmail.com>
2021-05-16 13:52:26 +0300

    changed name variable

--- logcallback.py
+++ logcallback.py
@@ -3,12 +3,12 @@ import json
 
 from tensorflow import keras
 
-from config import LOGS_DIR_CURRENT_MODEL, SAVE_CURRENT_MODEL, SAVE_MODEL_EVERY_ERA
+from config import LOGS_DIR_CURRENT_MODEL, SAVE_CURRENT_MODEL, SAVE_MODEL_EVERY_EPOCH
 
 
 class LogCallback(keras.callbacks.Callback):
     def __init__(self, model_save_path: str = SAVE_CURRENT_MODEL, logs_save_path: str = LOGS_DIR_CURRENT_MODEL,
-                 save_model_every_era: bool = SAVE_MODEL_EVERY_ERA) -> None:
+                 save_model_every_era: bool = SAVE_MODEL_EVERY_EPOCH) -> None:
         """
         Logging all training metrics to a json file and saving the model every epoch.
 
@@ -20,17 +20,17 @@ class LogCallback(keras.callbacks.Callback):
         self.model_save_path = model_save_path
         self.log_file = os.path.join(logs_save_path, 'train_logs.json')
         self.logs = {'epochs': []}
-        self.save_model_every_era = save_model_every_era
+        self.save_model_every_epoch = save_model_every_era
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.save_logs()
-        if self.save_model_every_era:
+        if self.save_model_every_epoch:
             self.model.save(os.path.join(self.model_save_path, 'last.h5'))
 
-    def save_logs(self):
+    def save_logs(self) -> None:
         with open(self.log_file, 'w') as file:
             json.dump(self.logs, file, indent=4)
 
@@ -43,5 +43,5 @@ class LogCallback(keras.callbacks.Callback):
             text.append('{}: {:.04f}'.format(key, float(value)))
         self.logs['epochs'].append('; '.join(text))
         self.save_logs()
-        if self.save_model_every_era:
+        if self.save_model_every_epoch:
             self.model.save(os.path.join(self.model_save_path, '{:03d}_epoch.h5'.format(1 + epoch)))
