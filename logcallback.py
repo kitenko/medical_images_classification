7f814e723f72d7021280f037cc8446dea7d57ced
андрей китенко <genialniy.kitenko@gmail.com>
2021-05-18 17:56:51 +0300

    added docking, typing

--- logcallback.py
+++ logcallback.py
@@ -34,7 +34,14 @@ class LogCallback(keras.callbacks.Callback):
         with open(self.log_file, 'w') as file:
             json.dump(self.logs, file, indent=4)
 
-    def on_epoch_end(self, epoch, logs=None):
+    def on_epoch_end(self, epoch: int, logs: dict = None) -> None:
+        """
+        This method prepares the data for writing to a json file and calls the method to save the json file.
+        If  = self.save_model_every_epoch = True, then the model is saved every epoch.
+
+        :param epoch: current epoch number.
+        :param logs: dictionary with metrics that are specified in the model, such as accuracy, precision or recall.
+        """
         text = ['epoch: {:03d}'.format(epoch + 1)]
         for key, value in logs.items():
             if key not in self.logs:
